5e4a3eef40d6195276dfa1cb3f1a6ddfa01a4ef2
michalnaoko <michal.pycia@naokko.com>
2023-07-14 21:13:01 +0200

    css

---
+++ job.py
@@ -0,0 +1,6 @@
+class Job():
+    def __init__(self, id, title, location, salary) -> None:
+        self.id = id
+        self.title = title
+        self.location = location
+        self.salary = salary
